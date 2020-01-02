8bd84906c77a3b670afbb4e3f729758fcfddec42
Benjamin Griffiths <drjamin1990@gmail.com>
2020-01-02 15:39:02 +0000

    implemented firebase utils included ability to store auth user in firebase database

---
+++ src/test.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('IYqqBC792GLNkwuchij2').collection('cartItems').doc('0mJ2raFby13kKG3Ex1JX
+');
+firestore.doc('/users/IYqqBC792GLNkwuchij2/cartItems/0mJ2raFby13kKG3Ex1JX');
+firestore.collection('/users/IYqqBC792GLNkwuchij2/cartItems');
